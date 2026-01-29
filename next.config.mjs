9600b49a02f3d3387294e6989d8f2d0ad03a945b
RAVI08RR <ravisoni08rrrr@gmail.com>
2026-01-29 14:01:33 +0530

    cross issue fix vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: false,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all API routes
+        source: '/backend-api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { 
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization',
+          },
+        ],
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
         source: '/backend-api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL || 'https://ccb7878ed7f8.ngrok-free.app'}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'https://a36498aba6e6.ngrok-free.app'}/:path*`,
       },
     ];
   },
