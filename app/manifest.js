1bbf20b216b7384a4c61566cd05c6e2dab8f469b
RAVI08RR <ravisoni08rrrr@gmail.com>
2026-05-22 18:46:54 +0530

    Update PWA manifest to explicitly use logo.png for app icons

--- app/manifest.js
+++ app/manifest.js
@@ -9,19 +9,19 @@ export default function manifest() {
     theme_color: '#000000',
     icons: [
       {
-        src: '/icon.png',
+        src: '/logo.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'any maskable',
       },
       {
-        src: '/icon.png',
+        src: '/logo.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any maskable',
       },
       {
-        src: '/apple-icon.png',
+        src: '/logo.png',
         sizes: '180x180',
         type: 'image/png',
       }
